232e4ea57b24406a57104331dc72bf371c9dd78a
joseotaviopc <carvalho.jose@gmail.com>
2021-12-16 12:12:01 -0300

    first commit - class boulder

---
+++ boulder.js
@@ -0,0 +1,30 @@
+/*
+Boulder
+    name
+    grade
+    url_image
+    general_details
+    beta
+    url_video
+    cordinates
+    conditions
+
+*/
+
+class Boulder {
+
+    constructor(name, grade, url_image, general_details, beta, url_video, coordinates, conditions) {
+        this._id = 0
+        this.name = name
+        this.grade = grade
+        this.url_image = url_image
+        this.general_details = general_details
+        this.beta = beta
+        this.url_video = url_video
+        this.coordinates = coordinates
+        this.conditions = conditions
+    }
+
+    
+
+}
