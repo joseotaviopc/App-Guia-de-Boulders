99b0ca16b9df12794084bdb1dfb34585741b5a90
joseotaviopc <carvalho.jose@gmail.com>
2021-12-17 19:18:15 -0300

    add createBoulder and updateBoulder

--- boulder.js
+++ boulder.js
@@ -11,9 +11,11 @@ Boulder
 
 */
 
+const boulders = [];
 class Boulder {
 
     constructor(name, grade, url_image, general_details, beta, url_video, coordinates, conditions) {
+        
         this._id = 0
         this.name = name
         this.grade = grade
@@ -25,6 +27,35 @@ class Boulder {
         this.conditions = conditions
     }
 
-    
+
+    // deleteBoulder(name) {
+
+    // }
+
+    updateBoulder(
+        name, grade, url_image, general_details, beta, url_video, coordinates, conditions) {
+        
+            if(boulders.find(boulder => boulder.name === name)){
+
+                this.name = name
+                this.grade = grade
+                this.url_image = url_image
+                this.general_details = general_details
+                this.beta = beta
+                this.url_video = url_video
+                this.coordinates = coordinates
+                this.conditions = conditions
+            } else {
+                console.log("Boulder não encontrado");
+            }
+        }
 
 }
+
+function createBoulder(
+    name, grade, url_image, general_details, beta, url_video, coordinates, conditions) {
+        
+        const boulder = new Boulder(name, grade, url_image, general_details, beta, url_video, coordinates, conditions);
+
+        boulders.push(boulder);
+    }
