9152f62e6d98e187432f4af6ab5a789bc7f119e0
joseotaviopc <carvalho.jose@gmail.com>
2022-02-16 20:51:36 -0300

    route post

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
 
-
+app.post('/post', function (req, res) {
+    res.send('Got a POST request');
+  });
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
